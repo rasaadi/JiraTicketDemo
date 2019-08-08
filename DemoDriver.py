3f25bed3ab638c0c9c90631aa5e7d71f6cee1899
saadira <rafsan.saadi@verizon.com>
2019-08-07 23:08:54 -0700

    - Implemented common func create_new_issue - implemented get_issue_id

--- DemoDriver.py
+++ DemoDriver.py
@@ -4,16 +4,16 @@ from JiraHandler import JiraHandler
 class DemoDriver:
     myJira = JiraHandler(username='rafsan.saadi', password='Pass!23')
     print(myJira.get_projects())
-    # print(myJira.get_issue_by_id('CCI-2'))
-    # myJira.crate_new_issue_2(project='CCI', summary='New issue created through JIRA client',
-    #                           description='This is the description for the new JIRA issue',
-    #                           issuetype={'name': 'Bug'})
 
     issue_dict = {
-        'project': {'id': 10000},
-        'summary': 'New issue from jira-python',
-        'description': 'Look into this one',
-        'issuetype': {'name': 'Bug'},
+        'project': {'key': "CCI"},
+        'summary': "New issue created through JIRA client-9",
+        'description': "This is the description for the new JIRA issue",
+        'issuetype': {'name': "Bug"}
     }
+    myJira.create_new_issue(fields=issue_dict)
+    myJira.create_new_issue(project={'key': "CCI"}, summary="New issue created through JIRA client-11",
+                            description="This is the description for the new JIRA issue-11", issuetype={'name': "Bug"})
 
-    myJira.crate_new_issue_3(issue_dict)
+    print(myJira.get_issue_by_id('CCI-15'))
+
--- JiraHandler.py
+++ JiraHandler.py
@@ -42,12 +42,7 @@ class JiraHandler:
                 project_list.append({'KEY': project.key, 'NAME': project.name, 'ID': project.id})
         return project_list
 
-    def choose_field_type(self, fields=None, **fieldargs):
-        if fields is not None:
-            return {'fielfs': fields}
-        return {'fields': fieldargs}
-
-    def crate_new_issue(self, fields=None):
+    def crate_new_issue_with_fields(self, fields=None):
         if fields is not None:
             try:
                 new_issue = self.JIRA_CLIENT.create_issue(fields)
@@ -58,7 +53,7 @@ class JiraHandler:
         else:
             raise JiraException("Did not specify ISSUE details")
 
-    def create_new_issue(self, **fieldargs):
+    def create_new_issue_with_fieldargs(self, **fieldargs):
         if len(fieldargs) != 4:
             raise JiraException("Provide details to create new issue")
 
@@ -66,30 +61,46 @@ class JiraHandler:
             new_issue = self.JIRA_CLIENT.create_issue(project=fieldargs['project'], summary=fieldargs['summary'],
                                                       description=fieldargs['description'],
                                                       issuetype=fieldargs['issuetype'])
-            return new_issue
             print("Jira issue created: {}".format(new_issue))
         except:
             raise JiraException("Not enough details was specified to create the issue")
+        return new_issue
 
+    def create_new_issue(self, fields=None, **fieldargs):
+        if fields is not None and len(fieldargs) == 0:
+            self.crate_new_issue_with_fields(fields)
+        elif len(fieldargs) == 4 and fields == None:
+            self.create_new_issue_with_fieldargs(**fieldargs)
+        else:
+            raise JiraException("Cannot create jira ISSUE with provided info")
 
-        # def get_issue_by_id(self, issueID):
-        #     issue = JIRA.issue(issueID)
-        #     return issue
+    def get_issue_by_id(self, issueID):
+        issue_details = []
+        try:
+            issue = self.JIRA_CLIENT.issue(issueID)
+        except:
+            raise JiraException("Failed to find the target ISSUE")
+
+        issue_details.append(
+            {'PROJECT': issue.fields.project, 'SUMMARY': issue.fields.summary, 'DESCRIPTION': issue.fields.description,
+             'STATUS': issue.fields.status})
+
+        return issue_details
+
 
         "https://github.com/r3ap3rpy/python/blob/master/JiraAPIWrapper.py"
         "https://www.youtube.com/watch?v=Nh01NDSRG1s"
         "https://jira.readthedocs.io/en/latest/examples.html#quickstart"
 
-
-if __name__ == '__main__':
-    myJira = JiraHandler(username='rafsan.saadi', password='Pass!23')
-    print(myJira.get_projects())
-    issue_dict = {
-        'project': {'key': "CCI"},
-        'summary': "New issue created through JIRA client-5",
-        'description': "This is the description for the new JIRA issue",
-        'issuetype': {'name': "Bug"}
-    }
-    myJira.crate_new_issue(fields=issue_dict)
-    myJira.create_new_issue(project={'key': "CCI"}, summary="New issue created through JIRA client-6",
-                            description="This is the description for the new JIRA issue", issuetype={'name': "Bug"})
+# if __name__ == '__main__':
+#     myJira = JiraHandler(username='rafsan.saadi', password='Pass!23')
+#     print(myJira.get_projects())
+#     issue_dict = {
+#         'project': {'key': "CCI"},
+#         'summary': "New issue created through JIRA client-5",
+#         'description': "This is the description for the new JIRA issue",
+#         'issuetype': {'name': "Bug"}
+#     }
+#     myJira.crate_new_issue_with_fields(fields=issue_dict)
+#     myJira.create_new_issue_with_fieldargs(project={'key': "CCI"}, summary="New issue created through JIRA client-6",
+#                                            description="This is the description for the new JIRA issue", issuetype={'name': "Bug"})
